2b8ac58522445d6aaa0ed7d98ba59da62ad2540e
Louis Ritchie <louiscritchie@gmail.com>
2018-06-27 08:48:48 -0700

    first pass at using mutex/conditional values in rw.c

--- rw.c
+++ rw.c
@@ -14,6 +14,9 @@
  */
 
 static resource_t data;
+static pthread_mutex_t mutex;
+static pthread_cond_t cond;
+static int reading, writing;
 
 void initialize_readers_writer() {
     /*
@@ -21,17 +24,49 @@ void initialize_readers_writer() {
      * synchronization.
      */
     init_resource(&data, "my_data");
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond, NULL);
+    reading = 0;
+    writing = 0;
 }
 
 
 void rw_read(char *value, int len) {
     printf("[rw_read] Value: %s, len: %d\n", value, len);
     print_stats(&data);
+
+    pthread_mutex_lock(&mutex);
+    while (writing) {
+        printf("here");
+        pthread_cond_wait(&cond, &mutex);
+    }
+    reading++;
+    pthread_mutex_unlock(&mutex);
+
     read_resource(&data, value, len);
+
+    pthread_mutex_lock(&mutex);
+    reading--;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
 }
 
 
 void rw_write(char *value, int len) {
     printf("[rw_write] Value: %s, len: %d\n", value, len);
+    print_stats(&data);
+
+    pthread_mutex_lock(&mutex);
+    while (reading) {
+        pthread_cond_wait(&cond, &mutex);
+    }
+    writing++;
+    pthread_mutex_unlock(&mutex);
+
     write_resource(&data, value, len);
+
+    pthread_mutex_lock(&mutex);
+    writing--;
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
 }
