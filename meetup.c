825de4641294e083707277320c55eec6e65726cb
Louis Ritchie <louiscritchie@gmail.com>
2018-06-27 20:22:05 -0700

    initialize vars in meetup.c; remove logs from rw.c

--- meetup.c
+++ meetup.c
@@ -12,6 +12,14 @@
  * Declarations for barrier shared variables -- plus concurrency-control
  * variables -- must START here.
  */
+
+static resource_t codeword;
+
+static sem_t semaphore;
+// sem_post - increment
+// sem_wait - decrement, if 0 then block until > 0.
+// sem_init(&semaphore, 1, 0);
+
 void initialize_meetup(int n, int mf) {
     char label[100];
     int i;
@@ -26,9 +34,14 @@ void initialize_meetup(int n, int mf) {
      * Initialize the shared structures, including those used for
      * synchronization.
      */
+    sem_init(&semaphore, 1, 0);
+    init_resource(&codeword, "codeword");
 }
 
 
 void join_meetup(char *value, int len) {
-    printf("NOTHING IMPLEMENTED YET FOR join_meetup\n");
+    printf("[join_meetup] Value: %s, len: %d\n", value, len);
+    print_stats(&codeword);
+
+    write_resource(&codeword, value, len);
 }
